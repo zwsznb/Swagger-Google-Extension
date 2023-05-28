ae35f44d8e571ce877c1a46c4bec376dc47f56d0
“zwsznb” <562009449@qq.com>
2023-05-28 21:43:36 +0800

    feat: 初始化项目

---
+++ scripts/content.js
@@ -0,0 +1,138 @@
+// var data = Mock.mock({
+//     // 属性 list 的值是一个数组，其中含有 1 到 10 个元素
+//     'list|1-10': [{
+//         // 属性 id 是一个自增数，起始值为 1，每次增 1
+//         'id|+1': 1
+//     }]
+// })
+// // 输出结果
+// console.log(JSON.stringify(data, null, 4))
+
+let swagger_json_path = location.origin + "/swagger/v1/swagger.json";
+http.get(swagger_json_path, function (err, result) {
+    if (err) {
+        console.log("swagger json地址请求失败");
+    }
+    receive_paths(result.paths, result.components.schemas);
+});
+function receive_paths(paths, object_param) {
+    let url_param_map = [];
+    for (let url in paths) {
+        let url_node = null;
+        url_node = handle_param(url, paths, object_param);
+        url_param_map.push({
+            url: url,
+            params: url_node
+        });
+    }
+    console.log(url_param_map);
+
+}
+//把所有的请求参数合并
+//统一格式 { url:/path,param:[{name:name,type:string,in:"query or body"}] }
+//"parameters": [
+// {
+//     "name": "key",
+//     "in": "query",
+//     "schema": {
+//       "type": "string"
+//     }
+//   }
+// ],
+
+function handle_param(url, paths, schemas) {
+    let method = get_request_method(paths, url);
+    let url_node = null;
+    if (method === "GET") {
+        let get = paths[url].get;
+        url_node = handle_get_param(get, schemas);
+    }
+    if (method === "POST") {
+        let post = paths[url].post;
+        url_node = handle_post_param(post, schemas);
+    }
+    return url_node;
+}
+function handle_get_param(param_source, schemas) {
+    let url_param = handle_url_param(param_source);
+    let body_param = handle_body_param(param_source, schemas);
+    //合并 
+    //TODO 可能会有问题，得留意
+    let params = {};
+    Object.assign(params, url_param, body_param);
+    return params;
+}
+function handle_post_param(param_source, schemas) {
+    let url_param = handle_url_param(param_source);
+    let body_param = handle_body_param(param_source, schemas);
+    //合并
+    //TODO 可能会有问题，得留意
+    let params = {};
+    Object.assign(params, url_param, body_param);
+    return params;
+}
+//处理url上的参数
+function handle_url_param(param_source) {
+    let url_params = [];
+    if (!param_source.parameters) {
+        return url_params;
+    }
+    let params = param_source.parameters;
+    for (let param in params) {
+        let url_param = {
+            name: param.name,
+            in: param.in,
+            type: params[param].schema.type
+        };
+        if (!url_param.name) {
+            continue;
+        }
+        url_params.push(url_param);
+    }
+    return url_params;
+}
+//post的object参数在components-schemas
+//如果是单个参数会放到paths-parameter里面，默认url的参数，不考虑
+//TODO 没有判断对象的类型，这里默认就是object
+function handle_body_param(param_source, schemas) {
+    if (!param_source.requestBody) {
+        return [];
+    }
+    let body_dto = param_source.requestBody.content["application/json"]["schema"]["$ref"];
+    let temp_arr = body_dto.split("/");
+    //TODO 忘了怎么改了
+    let dto_name = "";
+    for (let index in temp_arr) {
+        dto_name = temp_arr[index];
+    }
+    let properties = schemas[dto_name].properties;
+    let body_params = [];
+    for (let param_name in properties) {
+        let body_param = {
+            name: param_name,
+            in: "body",
+            type: properties[param_name].type
+        };
+        if (!body_param.name) {
+            continue;
+        }
+        body_params.push(body_param);
+    }
+    return body_params;
+}
+function mock_url_data() {
+
+}
+function mock_body_data() {
+
+}
+//TODO query和body都有参数的请求
+
+function get_request_method(paths, url) {
+    if (paths[url].get) {
+        return "GET";
+    }
+    if (paths[url].post) {
+        return "POST";
+    }
+}
