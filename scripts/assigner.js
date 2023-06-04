1e6e0da6c4d308e7ad059c6f070c5d035ccf0aad
“zwsznb” <562009449@qq.com>
2023-06-04 09:43:16 +0800

    refactor: 分离mock数据生成函数

--- scripts/assigner.js
+++ scripts/assigner.js
@@ -1,33 +1,22 @@
 //先只支持处理string和int和double
 // { url:/path,params:[{name:name,type:string,in:"query or body"}] }
 //数据结构在原来的基础上加上两个属性吧，is_set_value(是否赋值)，value(值)
-//TODO 整理，将mock数据分离到mock_data.js文件中
 function auto_assigner(url_and_params) {
     params_cycle(url_and_params, (params) => {
         if (!params.is_set_value) {
             //integer or number
             if (params.type === "integer" || params.type === "number") {
-                params.value = Mock.mock({
-                    "number|1-100": 100
-                }).number;
+                params.value = mock_number(1, 100);
             }
             if (params.type === "string") {
-                params.value = Mock.mock('@word(3, 10)');
+                params.value = mock_string(3, 10);
             }
             if (params.type === "boolean") {
-                params.value = Mock.mock({
-                    "boolean|1-2": true
-                }).boolean;
+                params.value = mock_bool();
             }
             if (params.type === "array") {
                 if (params.items_type === "string") {
-                    params.value = Mock.mock({
-                        "array|2-5": [
-                            Mock.mock('@word(3, 10)'),
-                            Mock.mock('@word(3, 10)'),
-                            Mock.mock('@word(3, 10)')
-                        ]
-                    }).array;
+                    params.value = mock_array();
                 }
             }
             params.is_set_value = true;
@@ -39,11 +28,11 @@ function auto_assigner(url_and_params) {
 //时间赋值器,将时间设置为毫秒级的时间戳
 function date_time_assigner(url_and_params) {
     params_cycle(url_and_params, (params) => {
-        let time = "Time";
-        let date = "Date";
-        if (params.name.endsWith(time) || params.name.endsWith(date)) {
+        let time = "time";
+        let date = "date";
+        if (params.name.toLowerCase().endsWith(time) || params.name.toLowerCase().endsWith(date)) {
             //随机时间戳
-            let random = new Date(Mock.mock('@datetime()')).getTime();
+            let random = mock_timestamp();
             params.value = random;
             params.is_set_value = true;
         }
@@ -69,8 +58,8 @@ function page_assigner(url_and_params) {
 //id赋值器
 function id_assigner(url_and_params) {
     params_cycle(url_and_params, (params) => {
-        if (params.name.includes("id")) {
-            params.value = Mock.mock('@guid()').toString();
+        if (params.name.toLowerCase().includes("id")) {
+            params.value = mock_guid();
             params.is_set_value = true;
         }
     })
@@ -80,8 +69,8 @@ function id_assigner(url_and_params) {
 //身份证赋值器
 function id_card_assigner(url_and_params) {
     params_cycle(url_and_params, (params) => {
-        if (params.name === "idsNo" || params.name === "IdsNo") {
-            params.value = Mock.mock(/^[1-8]{2}[0-9]{4}[0-9]{4}((0[1-9]{1})|(1[0-2]{1}))((0[1-9]{1})|(1[0-9]{1})|(2[0-9]{1})|(3[0-1]{1}))[0-9]{3}[0-9xX]{1}$/);
+        if (params.name.toLowerCase() === "IdsNo".toLowerCase()) {
+            params.value = mock_idcard();
             params.is_set_value = true;
         }
     });
@@ -90,8 +79,8 @@ function id_card_assigner(url_and_params) {
 //手机号赋值器
 function phone_assigner(url_and_params) {
     params_cycle(url_and_params, (params) => {
-        if (params.name.includes("phone") || params.name.includes("Phone")) {
-            params.value = Mock.mock(/^1[34578]\d{9}$/);
+        if (params.name.toLowerCase().includes("phone")) {
+            params.value = mock_phone();
             params.is_set_value = true;
         }
     })
---
+++ scripts/mock_data.js
@@ -0,0 +1,45 @@
+//模拟身份证
+function mock_idcard() {
+    return Mock.mock(/^[1-8]{2}[0-9]{4}[0-9]{4}((0[1-9]{1})|(1[0-2]{1}))((0[1-9]{1})|(1[0-9]{1})|(2[0-9]{1})|(3[0-1]{1}))[0-9]{3}[0-9xX]{1}$/);
+}
+
+function mock_phone() {
+    return Mock.mock(/^1[34578]\d{9}$/);
+}
+
+function mock_guid() {
+    return Mock.mock('@guid()').toString();
+}
+
+function mock_date() {
+    return Mock.mock('@datetime()');
+}
+function mock_timestamp() {
+    return new Date(mock_date()).getTime();
+}
+
+function mock_bool() {
+    return Mock.mock({
+        "boolean|1-2": true
+    }).boolean;
+}
+function mock_number(min, max) {
+    let name = `number|${min}-${max}`;
+    return Mock.mock({
+        [name]: 100
+    }).number;
+}
+
+function mock_string(min, max) {
+    return Mock.mock(`@word(${min}, ${max})`);
+}
+
+function mock_array() {
+    return Mock.mock({
+        "array|2-5": [
+            mock_string(3, 10),
+            mock_string(3, 10),
+            mock_string(3, 10)
+        ]
+    }).array;
+}
